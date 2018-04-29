d0bd729b6f32bed86ea53d39a9ec6e790ca560c3
Thomas de Lange <delange.thomas@gmail.com>
2018-04-29 22:55:04 +0200

    Personificeren doc

--- setup.py
+++ setup.py
@@ -12,14 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='Web Crawler',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Web Crawler to analyze sentiment of webpages',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
+    author='Thomas de Lange',
+    author_email='delange.thomas@gmail.com',
     url='https://github.com/kennethreitz/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
