52fa630281edce9322d8afd0884cb01dd15c4ea0
Thomas de Lange <delange.thomas@gmail.com>
2018-05-11 17:44:52 +0200

    Eerste test van de analyzer
    Nog zonder dat er gezocht word naar een woord als facebook

--- App/Classes/FileManager.py
+++ App/Classes/FileManager.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 import json
+import bz2
+import tarfile
 
 class FileManager(object):
     """
     File Manager regeld alles wat nodig is om tweets uit de dataset te halen.
     """
-    
-    def getTweets(input_file):
-        """geeft terug de tweets uit een gegeven input_file"""
+    def getTweets(file_name, tar):
+        """
+        Geeft terug de tweets uit een gegeven input_file die in het bz2 format is.
+        """
         tweets = []
+        bz2_file = tar.extractfile(file_name)
 
-        for line in open(input_file, 'r'):
+        for line in bz2.open(bz2_file, 'rt'):
             tweets.append(json.loads(line))
 
         return tweets
--- App/core.py
+++ App/core.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import helpers
 import json
+import os
+import tarfile
+import bz2
 
 from pprint import pprint
+# from pathlib import Path
 from Classes.FileManager import FileManager
 from Classes.BColors import BColors
 from Classes.TweetAnalyzer import TweetAnalyzer
@@ -10,25 +14,35 @@ from Classes.TweetAnalyzer import TweetAnalyzer
 def start():
     """Start the App"""
     """ Maak twee arrays met positive en negative worden. """
-    negative_list = helpers.getWordsFromLexicons('sentiment_lexicons/negative-words.txt')
-    positive_list = helpers.getWordsFromLexicons('sentiment_lexicons/positive-words.txt')
+    negative_list = helpers.getWordsFromLexicons('sentiment-lexicons/negative-words.txt')
+    positive_list = helpers.getWordsFromLexicons('sentiment-lexicons/positive-words.txt')
 
-    tweets = FileManager.getTweets('test_data/30.json')
+    top_dir = 'test_data' # Locatie van de twitter data
 
-    for tweet in tweets: # Loop door te tweets
-        if 'lang' in tweet: # Kijk of in de tweet de taal word aangegeven
-            if tweet['lang'] == 'en': # Kijk of de taal in de tweet engels is
-                tweet_sentiment = TweetAnalyzer.getSentiment(tweet, positive_list, negative_list)
+    for root, dirs, files in os.walk(top_dir, topdown=False): # Loop door de bestanden in test_data
+        for name in files:
+            if name.endswith(".tar"):
+                tar = tarfile.open(root + "/" + name, 'r')
+                file_names = tar.getnames()
 
-                for date, value in tweet_sentiment.items():
-                    with open('results/json_result.json', 'r') as f: # Open de bestaande uitkomsten
-                        json_data = json.load(f)
+                for file_name in file_names:
+                    if file_name.endswith(".json.bz2"): # Kijk of het wel een .json.bz2 bestand is
+                        tweets = FileManager.getTweets(file_name, tar)
 
-                        if date in json_data:
-                            json_data[date]["positive_words"] += value['positive_words']
-                            json_data[date]["negative_words"] += value['negative_words']
-                        else:
-                            json_data  = tweet_sentiment
+                        for tweet in tweets: # Loop door te tweets
+                            if 'lang' in tweet: # Kijk of in de tweet de taal word aangegeven
+                                if tweet['lang'] == 'en': # Kijk of de taal in de tweet engels is
+                                    tweet_sentiment = TweetAnalyzer.getSentiment(tweet, positive_list, negative_list)
 
-                    with open('results/json_result.json', 'w') as fp: # Schrijf de uitkomsten opnieuw weg
-                        json.dump(json_data, fp)
+                                    for date, value in tweet_sentiment.items():
+                                        with open('results/json_result.json', 'r') as f: # Open de bestaande uitkomsten
+                                            json_data = json.load(f)
+
+                                            if date in json_data:
+                                                json_data[date]["positive_words"] += value['positive_words']
+                                                json_data[date]["negative_words"] += value['negative_words']
+                                            else:
+                                                json_data.update(tweet_sentiment)
+
+                                        with open('results/json_result.json', 'w') as fp: # Schrijf de uitkomsten opnieuw weg
+                                            json.dump(json_data, fp)
