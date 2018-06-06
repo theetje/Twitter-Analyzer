355b778d607b16f2eb8e539d00499c1b4de4b5cd
Thomas de Lange <delange.thomas@gmail.com>
2018-06-06 14:39:26 +0200

    Working Analyzer
    Code is a big mess.

--- Analyzer/core.py
+++ Analyzer/core.py
@@ -1,34 +1,86 @@
 # -*- coding: utf-8 -*-
+# helpers
+import csv
 import helpers
-import json
-import os
-import tarfile
-import bz2
-import string
-
 from pprint import pprint
 
+# models
 from Models.base import Base
 from Models.Tweet import Tweet
 
+# sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
+# statsmodels numpy, matplotlib and pandas for analysis
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 def start():
     """Start the Data Analyzer"""
 
-    top_dir = 'test_data' # Locatie van de twitter data
-    word_to_look_for = 'ibm' # woord waar je jaar gaat zoeken in underscore
+    word_to_analyze = 'ibm' # de database naam
+    maxlag = 4
+    plot_figure = False
 
-    engine = create_engine('sqlite:///' + word_to_look_for + '.sqlite')
+    engine = create_engine('sqlite:///' + word_to_analyze + '.sqlite')
     session = sessionmaker()
     session.configure(bind=engine)
     Base.metadata.create_all(engine)
 
     s = session()
 
-    for date, text in s.query(Tweet.date, Tweet.text):
-        print("On " + str(date) + " the following was tweeted: ")
-        print(text)
+    # Get the delta X sentiment score per day
+    Xt_dict = helpers.getXFromData(s, Tweet)
+    # Get the Return on stock for a given period from the dataset
+    Rt_dict = helpers.getRFromCSV('2017/10/01',
+                                        '2017/12/31',
+                                        'data/stock/historical-quotes-'
+                                        + word_to_analyze
+                                        + '.csv')
+
+    combined_results = helpers.combineRtandXt(Xt_dict, Rt_dict)
+
+    result = sm.tsa.stattools.grangercausalitytests(combined_results, 4, addconst=True, verbose=True)
+    exit()
+
+
+    ln_Rt_dict = helpers.getRtDictFromCSV('2017/10/01',
+                                        '2017/12/31',
+                                        'data/stock/historical-quotes-'
+                                        + word_to_analyze
+                                        + '.csv')
+
+    pos_neg_dict = helpers.getPositiveNegativePerDay(s, Tweet)
+
+    pprint(pos_neg_dict)
+    exit()
+    Xt_dict = helpers.getXtnormalized(pos_neg_dict)
+    combined_dicts = helpers.combineRtandXt(Xt_dict, Rt_dict)
+
+    if plot_figure:
+        Xt_df = pd.DataFrame(list(combined_dicts[0].items()), columns=['Date', 'Xt'])
+        Xt_df['Date'] = pd.to_datetime(Xt_df['Date'])
+
+        Rt_df = pd.DataFrame(list(combined_dicts[1].items()), columns=['Date', 'Rt'])
+        Rt_df['Date'] = pd.to_datetime(Rt_df['Date'])
+
+        Xt_df = Xt_df.sort_values('Date', ascending=True)
+        plt.plot(Xt_df['Date'], Xt_df['Xt'])
+        plt.xticks(rotation='horizontal')
+
+        Rt_df = Rt_df.sort_values('Date', ascending=True)
+        plt.plot(Rt_df['Date'], Rt_df['Rt'])
+        plt.xticks(rotation='horizontal')
+
+        plt.show()
+
+    Xt_tuples = list(combined_dicts[0].items())
+    Rt_tuples = list(combined_dicts[1].items())
+
+    Xt_list = [x[1] for x in Xt_tuples]
+    Rt_list = [r[1] for r in Rt_tuples]
+
+    result = sm.tsa.stattools.grangercausalitytests([[Xt_list[i], Rt_list[i]] for i in range(0, len(Xt_list))], maxlag, addconst=True, verbose=True)
--- Analyzer/helpers.py
+++ Analyzer/helpers.py
@@ -1,17 +1,135 @@
-""" Return een array met daarin de woorden voor analyse. """
-def getWordsFromLexicons(input_file):
-    f = open(input_file)
-    temp_array = []
+import csv
+from math import log as log
+from datetime import datetime
+from collections import OrderedDict
 
-    for lines in f:
-        if ';' not in lines: # haal de comments weg
-            temp_array.append(lines.rstrip())
+""" From %Y/%m/%d to %Y-%m-%d. """
+def convertDate(date):
+    date.strptime(start_date, "%Y/%m/%d")
+    return date.strftime('%Y-%m-%d')
 
-    return temp_array
+""" get the data from the sql lite database into a dictionary. """
+def getPositiveNegativePerDay(session, db_object):
+    temp_dict = OrderedDict()
 
-def tweetConstraint(tweet, language, key_word):
-    if 'lang' in tweet: # Kijk of in de tweet de taal word aangegeven
-        if tweet['lang'] == language: # Kijk of de taal in de tweet engels is
-            # Je kan hier kijken of je controleerd of er een geldige taal word opgegeven.
-            if key_word in tweet['text'].lower():
-                return tweet
+    for positive_words, negative_words, date in session.query(db_object.positive_words, db_object.negative_words, db_object.date):
+        str_date = date.strftime('%Y/%m/%d')
+
+        if str_date in temp_dict:
+            temp_dict[str_date][0] += positive_words
+            temp_dict[str_date][1] += negative_words
+        else:
+            temp_dict[str_date] = [positive_words, negative_words]
+
+    # Retrun a sorted dict, sorted by key.
+    return {k: temp_dict[k] for k in sorted(temp_dict)}
+
+""" Get the delta of the pos - neg ."""
+def getXFromData(session, db_object):
+    result_dict = {}
+    prev_day_sentiment = None
+    input_dict = getPositiveNegativePerDay(session, db_object)
+
+    for key, value in input_dict.items():
+        day_sentiment = value[0] - value[1]
+
+        if prev_day_sentiment is not None:
+            result_dict.update({key: day_sentiment - prev_day_sentiment})
+        prev_day_sentiment = float(day_sentiment)
+
+    return result_dict
+
+""" Return the Rt value from a CSV. """
+def getRFromCSV(start_date, end_date, file_dir):
+    with open(file_dir) as csvfile:
+        reader = csv.DictReader(csvfile)
+        _dict = OrderedDict()
+        start_date_object = datetime.strptime(start_date, "%Y/%m/%d")
+        end_date_object = datetime.strptime(end_date, "%Y/%m/%d")
+
+        # Lees de datum en slot stand in een dict
+        for row in reader:
+            stock_date_object = datetime.strptime(row['date'], "%Y/%m/%d")
+            if stock_date_object <= end_date_object and stock_date_object >= start_date_object:
+                _dict.update({row['date']:row['close']})
+
+        # sorteer de dict op datum
+        sorted_end_of_day = {k: _dict[k] for k in sorted(_dict)}
+        Rt_dict = {}
+        prev_value = None
+
+        for key, value in sorted_end_of_day.items():
+            if prev_value is not None:
+                Rt_dict.update({key: float(value) - prev_value})
+            prev_value = float(value)
+
+        return Rt_dict
+
+""" See if the date match and combine the results if they do."""
+def combineRtandXt(Xt_dict, Rt_dict):
+    result_array = []
+
+    for Rt_key, Rt_value in Rt_dict.items():
+        for Xt_key, Xt_value in Xt_dict.items():
+
+            if Rt_key == Xt_key:
+                result_array.extend([[Xt_value,  Rt_value]])
+
+
+    return result_array
+
+""" See if the date match and combine the results if they do. Return dict."""
+def combineRtandXtreturnDict(Xt_dict, Rt_dict):
+    Rt_result_dict = OrderedDict()
+    Xt_result_dict = OrderedDict()
+
+    for Rt_key, Rt_value in Rt_dict.items():
+        for Xt_key, Xt_value in Xt_dict.items():
+
+            if Rt_key == Xt_key:
+                Xt_result_dict.update({Xt_key: Xt_value})
+                Rt_result_dict.update({Rt_key: Rt_value})
+
+
+    return [Xt_result_dict, Rt_result_dict]
+
+""" Retrun the amount of rows. """
+def countRows(session, db_object):
+    return session.query(db_object).count()
+
+
+def getXtnormalized(input_dict):
+    prev_day_sentiment = None
+    result_dict = {}
+    for key, value in input_dict.items():
+        day_sentiment = log(value[0]) - log(value[1])
+
+        if prev_day_sentiment is not None:
+            result_dict.update({key: day_sentiment - prev_day_sentiment})
+        prev_day_sentiment = float(day_sentiment)
+
+    return result_dict
+
+""" Return the log Rt value from a CSV. """
+def getRtDictFromCSV(start_date, end_date, file_dir):
+    with open(file_dir) as csvfile:
+        reader = csv.DictReader(csvfile)
+        dict = OrderedDict()
+        start_date_object = datetime.strptime(start_date, "%Y/%m/%d")
+        end_date_object = datetime.strptime(end_date, "%Y/%m/%d")
+
+        # Lees de datum en slot stand in een dict
+        for row in reader:
+            stock_date_object = datetime.strptime(row['date'], "%Y/%m/%d")
+            if stock_date_object <= end_date_object and stock_date_object >= start_date_object:
+                dict.update({row['date']:row['close']})
+
+        prev_value = None
+        Rt_dict = {}
+        for key, value in dict.items():
+            if prev_value is not None:
+                Rt_dict.update({key: log(float(value)) - log(prev_value)})
+            prev_value = float(value)
+
+        # Retrun a sorted dict, sorted by key.
+        return {k: Rt_dict[k] for k in sorted(Rt_dict)}
